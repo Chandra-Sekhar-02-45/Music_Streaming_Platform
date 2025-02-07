5f15026799fced66f024edb022f7ab473cf1b3e4
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-07 23:40:18 +0530

    added the footer

--- music_platform/src/App.jsx
+++ music_platform/src/App.jsx
@@ -52,18 +52,18 @@ function App() {
         <div className='row'>
           <div className='d-flex flex-row justify-content-start card_0'>
             <div className='card_1 col-3'>
-              Library   
+            <h3>Library</h3>
             </div>
             <div className='card_1 col-6'>
-              Favouite Songs
+            <h3>Fav Songs</h3>
             </div>
-            <div className='card_1 col-3'>
-              Currently Playing
+            <div className='card_1 col-3' style={{ marginRight: '100px' }}>
+              <h3>Currently Playing</h3>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-fluid'>
+      <div className='container-fluid d-flex flex-column justify-content-end'>
         <div className='row'>
            <div className='col-12'>
               <div className='footer'>
@@ -79,7 +79,6 @@ function App() {
               </div>
            </div>
         </div>
-
       </div>
     </div>
   )
