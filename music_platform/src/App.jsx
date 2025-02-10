070e76663fad5bd10fa03d74c3f54c8cab8d2e82
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-10 19:47:29 +0530

    laogin is page is navigating cmpletely now

--- music_platform/src/App.jsx
+++ music_platform/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import './index.css'
 import { LandingPage } from './LandingPage'
 import { Login } from './Login'
+import { Navbar } from './Navbar'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/login') {
+      setIsLoggedIn(true)
+    } else {
+      setIsLoggedIn(false)
+    }
+  }, [location])
 
   return (
   <>
+  {!isLoggedIn && <Navbar />}
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
