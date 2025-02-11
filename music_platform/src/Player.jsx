89465043a429c79dd730439a876d3b56c2d8566b
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-12 01:05:12 +0530

    time line progress is also addeed in the player

--- music_platform/src/Player.jsx
+++ music_platform/src/Player.jsx
@@ -16,6 +16,11 @@ export const Player = () => {
                   <i className="fa-solid fa-forward skip"></i>
                   <i className="fa-solid fa-repeat skip"></i>
               </div>
+              <div className='progress-container'>
+                  <span className='time current-time'>0:00</span>
+                  <input type="range" className="progress-bar" min="0" max="100" value="0" />
+                  <span className='time total-time'>3:45</span>
+             </div>
             </div>
          </div>
       </div>
