6b7e5fa53c031e3a00e75d82b09a3c51c04ec0cb
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-11 15:17:18 +0530

    Fav Music page is completed

--- music_platform/src/LandingPage.jsx
+++ music_platform/src/LandingPage.jsx
@@ -20,11 +20,12 @@ export const LandingPage = () => {
             <button className='selection_btn'>Songs</button>
             <button className='selection_btn'>Podcasts</button>
             </div>
+
             <div className='d-flex flex-column justify-content-start '>
                 <div className='d-flex flex-row justify-content-center'>
                   <div className="album_fav_card">
-                      <img src="public/albums/Animal.jpg" alt="Animal Album" className="fav_albums" />
-                    <h3 className="album_fav_text">Animal Telugu</h3>
+                      <img src="public/albums/Magadheera.jpg" alt="Magadheera Album" className="fav_albums" />
+                    <h3 className="album_fav_text">Magadheera</h3>
                     <div className="justify-content-center">
                         <button className="album_play">
                             <i className="fa-solid fa-play"></i>
@@ -72,7 +73,7 @@ export const LandingPage = () => {
 
                   <div className="album_fav_card" style={{ marginTop: '2px' }} onMouseEnter={() => setPlay(true)} onMouseLeave={() => setPlay(false)} >
                       <img src="public/albums/Animal.jpg" alt="Animal Album" className="fav_albums" />
-                    <h3 className="album_fav_text">Animal Hindi</h3>
+                    <h3 className="album_fav_text">Animal Telugu</h3>
                     <div className="justify-content-center">
                       {
                         play ? <button className="album_play">
@@ -83,12 +84,77 @@ export const LandingPage = () => {
                   </div>
                 </div>
             </div>
-            {/* <div>
-              <h1>Made For You</h1>
-              <div className='d-flex flex-row justify-content-start made_for_you_card'>
-                 <img src="public/albums/Animal.jpg" alt="Animal Album" className="made_for_you_albums d-flex justify-content-center" />
-             </div>
-            </div>  */}
+
+
+            <div>
+              <h1 className="made-for-you-title">Made For You</h1>
+              <p className='d-flex justify-content-end show-all'>Show All</p>
+              <div className="made-for-you">
+                <div className="made-for-you-container">
+                <div className="made-for-you-card">
+                    <img src="public/albums/hiiNana.jpg" alt="Hi Nana" className="made-for-you-album" />
+                    <p className="made-for-you-text">Hi Nana<br></br> <span >By Hesham Abdul Wahab</span> </p>
+                </div>
+                <div className="made-for-you-card">
+                    <img src="public/albums/Devara.jpg" alt="Devara Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Devara<br></br> <span >By Anirudh</span> </p>
+                </div>
+                  <div className="made-for-you-card">
+                    <img src="public/albums/allTimeHindhi.jpg" alt="allTimeHindhi" className="made-for-you-album" />
+                    <p className="made-for-you-text">All Time To 50 Songs Hindi<br></br> <span >By Music</span></p>
+                  </div>
+                  <div className="made-for-you-card">
+                    <img src="public/albums/GunturKaram.jpg" alt="Guntur Karam Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Guntur Kaaram<br></br> <span >By S. Thaman</span> </p>
+                  </div>
+                  <div className="made-for-you-card">
+                    <img src="public/albums/Animal-2.jpg" alt="Animal Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Animal ( Original Motion Picture Soundtrack ) . . .</p>
+                  </div>
+
+                  <div className="made-for-you-card">
+                    <img src="public/albums/justinBieber.jpg" alt="Another Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Justin Bieber My World 2.0<br></br><span >By Music</span> </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="made-for-you-title">Today's Biggest Hits</h1>
+              <p className='d-flex justify-content-end show-all'>Show All</p>
+              <div className="made-for-you">
+                <div className="made-for-you-container">
+                <div className="made-for-you-card">
+                    <img src="public/albums/GunturKaram.jpg" alt="Guntur Karam Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Guntur Kaaram<br></br> <span >By S. Thaman</span> </p>
+                  </div>
+                <div className="made-for-you-card">
+                    <img src="public/albums/hiiNana.jpg" alt="Hi Nana" className="made-for-you-album" />
+                    <p className="made-for-you-text">Hi Nana<br></br> <span >By Hesham Abdul Wahab</span> </p>
+                </div>
+                <div className="made-for-you-card">
+                    <img src="public/albums/Devara.jpg" alt="Devara Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Devara<br></br> <span >By Anirudh</span> </p>
+                </div>
+
+                <div className="made-for-you-card">
+                    <img src="public/albums/justinBieber.jpg" alt="Another Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Justin Bieber My World 2.0<br></br><span >By Music</span> </p>
+                  </div>
+                  <div className="made-for-you-card">
+                    <img src="public/albums/allTimeHindhi.jpg" alt="allTimeHindhi" className="made-for-you-album" />
+                    <p className="made-for-you-text">All Time To 50 Songs Hindi<br></br> <span >By Music</span></p>
+                  </div>
+                  <div className="made-for-you-card">
+                    <img src="public/albums/Animal-2.jpg" alt="Animal Album" className="made-for-you-album" />
+                    <p className="made-for-you-text">Animal ( Original Motion Picture Soundtrack ) . . .</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          
           </div>
           <div className='card_1 col-3' style={{ marginRight: '100px' }}>
             <h3>Currently Playing</h3>
