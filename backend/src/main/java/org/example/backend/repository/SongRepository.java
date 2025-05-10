6df473b60d8fe024dc5c3d2ab09fcc81cad80fec
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-10 16:47:43 +0530

    backend mistaken so deleted

--- backend/src/main/java/org/example/backend/repository/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.musicstream.backend.repository;
-
-import com.musicstream.backend.model.Song;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SongRepository extends MongoRepository<Song, String> {
-}
