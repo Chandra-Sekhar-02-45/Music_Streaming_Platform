6cd93a3a972e690303cff97879bbe2880dc13091
Hemanth Sai <nagiseishiro@Hemanths-MacBook-Air.local>
2025-05-10 17:04:32 +0530

    changed landing page

--- music_platform/src/LandingPage.jsx
+++ music_platform/src/LandingPage.jsx
@@ -9,9 +9,8 @@ export const LandingPage = () => {
     <div>
     <div className='container-fluid'>
       <div className='row'>
-        <div className='d-flex</div> flex-row justify-content-start card_0'>
-        
-        {/* Library Section */}
+        <div className='d-flex flex-row justify-content-start card_0'>
+
         <div className='card_1 col-3 '>
           <div className='library_container'>
 
@@ -162,7 +161,7 @@ export const LandingPage = () => {
           </div>
         </div>
 
-          {/* Music Section */}
+          
           <div className='card_1 col-6  selection'>
             <div className='d-flex flex-row justify-content-start'>
               <button className='selection_btn'>All</button>
@@ -305,7 +304,7 @@ export const LandingPage = () => {
             </div>
           </div>
 
-         {/* Currently Playing Section */}
+        
           <div className='card_1 col-3 currently_playing'>
             <div className='currently_playing_container'>
               <div className='content_wrapper d-flex flex-column justify-content-start align-items-center text-center'>
