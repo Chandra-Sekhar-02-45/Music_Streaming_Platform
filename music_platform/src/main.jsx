8e4de0d3b18e34543db78d05bbdb103586f3b33a
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-12 17:06:35 +0530

    deployment2

--- music_platform/src/main.jsx
+++ music_platform/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Music_Streaming_Platform">
        <App />
     </BrowserRouter>
   </StrictMode>,
