9a67f584e276cc5d44dbf2d68e524d6be34912f0
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-12 17:15:37 +0530

    deployment4

--- music_platform/vite.config.js
+++ music_platform/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',  // <- CHANGE THIS to your GitHub repo name
+  base: '/music_platform/',  // <- CHANGE THIS to your GitHub repo name
   server: {
     historyApiFallback: true,
   },
