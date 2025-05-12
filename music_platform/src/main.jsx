46392d5979c73723e0b939b4e6aaae3d349f24c1
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-12 17:45:18 +0530

    deployment9

--- music_platform/src/main.jsx
+++ music_platform/src/main.jsx
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
+// Dynamically set the basename based on the current URL path
+const basename = window.location.pathname.includes('Music_Streaming_Platform') 
+  ? '/Music_Streaming_Platform' 
+  : '/';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Music_Streaming_Platform">
-       <App />
+    <BrowserRouter basename={basename}>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
